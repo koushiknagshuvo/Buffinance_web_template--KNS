6e7bfb59078d04ac457d9074eb3069bf631a13fa
koushik nag shuvo <koushiknagshuvo@gmail.com>
2021-07-21 13:35:04 +0600

    add animation

--- JS/script.js
+++ JS/script.js
@@ -1,22 +1,7 @@
 // new WOW().init();  এইটা use করতে পারি বা নিচের টা , কারন এই ১ লাইন সব লাইনের সমান 
 
 
-// var wow = new WOW(
-//   {
-//     boxClass:     'wow',      // animated element css class (default is wow)
-//     animateClass: 'animated', // animation css class (default is animated)
-//     offset:       50,          // distance to the element when triggering the animation (default is 0)
-//     mobile:       true,       // trigger animations on mobile devices (default is true)
-//     live:         true,       // act on asynchronously loaded content (default is true)
-//     callback:     function(box) {
-//       // the callback is fired every time an animation is started
-//       // the argument that is passed in is the DOM node being animated
-//     },
-//     scrollContainer: null,    // optional scroll container selector, otherwise use window,
-//     resetAnimation: true,     // reset animation on end (default is true)
-//   }
-// );
-// wow.init();
+
 
 
 
@@ -62,4 +47,23 @@ $('.owl-carousel').owlCarousel({
     }
 })
 
+
 // OwlCarousel2 ============================================>
+
+
+var wow = new WOW(
+  {
+    boxClass:     'wow',      // animated element css class (default is wow)
+    animateClass: 'animated', // animation css class (default is animated)
+    offset:       50,          // distance to the element when triggering the animation (default is 0)
+    mobile:       true,       // trigger animations on mobile devices (default is true)
+    live:         true,       // act on asynchronously loaded content (default is true)
+    callback:     function(box) {
+      // the callback is fired every time an animation is started
+      // the argument that is passed in is the DOM node being animated
+    },
+    scrollContainer: null,    // optional scroll container selector, otherwise use window,
+    resetAnimation: true,     // reset animation on end (default is true)
+  }
+);
+wow.init();
